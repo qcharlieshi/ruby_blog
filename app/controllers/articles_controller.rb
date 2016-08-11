1fd8fb1a0c20ba6656aff3140932ba6a085a0f56
qcshi <charlies@live.com>
2016-08-10 22:15:10 -0400

    Homepage revamp

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def new
     @article = Article.new
